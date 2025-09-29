85ab8f284915e892629a5a0cba1406677ff3bbbf
ssmunkhbat <47767779+ssmunkhbat@users.noreply.github.com>
2025-09-29 15:07:39 +0800

    updated the schema

--- main/src/schema/schema.controller.ts
+++ main/src/schema/schema.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request, Post } from '@nestjs/common';
 import { SchemaService } from './schema.service';
 import { AuthGuard } from '../auth/auth.guard';
 
@@ -9,14 +9,20 @@ export class SchemaController {
 ) {}
 
   @UseGuards(AuthGuard)
+  @Get('/config/:name')
+  async getConfig(@Request() req): Promise<any[]> {
+    return await this.schemaService.getSchemaFields(req)
+  }
+
+  @UseGuards(AuthGuard)
   @Get('/list/:name')
   async getList(@Request() req): Promise<any[]> {
     return await this.schemaService.findAll(req)
   }
 
   @UseGuards(AuthGuard)
-  @Get('/config/:name')
-  async getConfig(@Request() req): Promise<any[]> {
-    return await this.schemaService.getSchemaFields(req)
+  @Post('/post/:name')
+  async post(@Request() req): Promise<any | undefined> {
+    return await this.schemaService.save(req.params.name, req.body)
   }
 }
--- main/src/schema/schema.service.ts
+++ main/src/schema/schema.service.ts
@@ -1,28 +1,19 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { SchemaAccessService, SchemaObjectConfig } from './schema.access.service';
+import { SetModule } from 'src/schemas/setModule.schema';
+import { DynamicModelService } from 'src/dynamic-model/dynamic-model.service';
 
 @Injectable()
 export class SchemaService {
   constructor(
-    private readonly schemaAccessService: SchemaAccessService
+    private readonly schemaAccessService: SchemaAccessService,
+    private dynamicModelService: DynamicModelService,
   ) {}
 
   checkSchema(modelName: string) {
     const checkModels = ['User', 'SetModule']
     if (!modelName || (checkModels.indexOf(modelName) === -1)) {
       throw new BadRequestException('Bad request!')
-    }
-  }
-
-  async findAll(req): Promise<any[]> {
-    try {
-      await this.checkSchema(req.params.name)
-      const collection = req.params.name
-      const dddd = await this.schemaAccessService.findAll(collection, ['name', 'password'], {})
-      return dddd
-    } catch (error) {
-      console.error('Error in findAll:', error);
-      throw error;
     }
   }
 
@@ -35,8 +26,34 @@ export class SchemaService {
       const columns = await this.schemaAccessService.getColumnsProps(schema)
       return columns
     } catch (error) {
-      console.error('Error in getSchemaFields:', error);
+      console.error('Error in schema -> getSchemaFields:', error);
       throw error;
     }
   }
+
+  async findAll(req): Promise<any[]> {
+    try {
+      await this.checkSchema(req.params.name)
+      const collection = req.params.name
+      const dddd = await this.schemaAccessService.findAll(collection, ['code', 'name', 'description'], {})
+      return dddd
+    } catch (error) {
+      console.error('Error in schema -> findAll:', error);
+      throw error;
+    }
+  }
+
+  async save(modelName, data): Promise<SetModule | undefined> {
+      try {
+        await this.checkSchema(modelName)
+        const found = await this.dynamicModelService.findOne(modelName, { name: data.name })
+        if (found) {
+          throw new BadRequestException('Өмнө бүртгэгдсэн байна.');
+        }
+        return await this.dynamicModelService.save(modelName, data)
+      } catch (error) {
+        console.error('Error in schema -> save:', error);
+        return undefined;
+      }
+    }
 }
