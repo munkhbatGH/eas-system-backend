e821446a43fb47038b2708f6b1059842ac7e690c
ssmunkhbat <47767779+ssmunkhbat@users.noreply.github.com>
2025-10-03 17:00:48 +0800

    updated

--- main/src/settings/settings.service.ts
+++ main/src/settings/settings.service.ts
@@ -16,6 +16,7 @@ export class SettingsService {
         {$unwind: { path: '$menuList', preserveNullAndEmptyArrays: true}},
         {$lookup: { from: 'setmenus', localField: 'menuList', foreignField: '_id', as: 'menu'}},
         {$unwind: { path: '$menu', preserveNullAndEmptyArrays: true}},
+        {$sort: { 'menu.order': 1 }},
         {$project: {
           _id: 1,
           menu: { _id: '$menu._id', parentId: '$menu.parentId', code: '$menu.code', name: '$menu.name', order: '$menu.order', moduleId: '$menu.moduleId', path: '$menu.path', icon: '$menu.icon' },
